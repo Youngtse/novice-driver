f5f156515c333d30722060bd653653f961866661
gen.yang <gen.yang@renmai-inc.com>
2016-07-18 19:08:22 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+// webpack.config.js
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    // entry point of our application
+    entry: './main.js',
+    // where to place the compiled bundle
+    output: {
+        path: __dirname,
+        filename: 'build.js'
+    },
+    module: {
+        // `loaders` is an array of loaders to use.
+        // here we are only configuring vue-loader
+        loaders: [
+            {
+                test: /\.vue$/, // a regex for matching all files that end in `.vue`
+                loader: 'vue'   // loader to use for matched files
+            },
+            {
+                // use babel-loader for *.js files
+                test: /\.js$/,
+                loader: 'babel',
+                // important: exclude files in node_modules
+                // otherwise it's going to be really slow!
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url',
+                query: {
+                    // limit for base64 inlining in bytes
+                    limit: 10000,
+                    // custom naming format if file is larger than
+                    // the threshold
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ],
+        // if you are using babel-loader directly then
+        // the babel config block becomes required.
+        babel: {
+            presets: ['es2015'],
+            plugins: ['transform-runtime']
+        }
+    },
+    vue: {
+        loaders: {
+            css: ExtractTextPlugin.extract("css"),
+            // you can also include <style lang="less"> or other langauges
+            less: ExtractTextPlugin.extract("css!less")
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ]
+}
